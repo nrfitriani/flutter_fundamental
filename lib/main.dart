3315c00bba083d5ca6dcdc678d80aadf9a95fb28
nrfitriani <54707827+nrfitriani@users.noreply.github.com>
2020-09-08 11:26:41 +0800

    Image widget

--- lib/main.dart
+++ lib/main.dart
@@ -336,7 +336,7 @@ class MyApp extends StatelessWidget {
 */
 
 //Stack and Align Widget
-void main() => runApp(MyApp());
+/*void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
@@ -476,6 +476,28 @@ class MyApp extends StatelessWidget {
               
             ))
           ],
+        ),
+      ),
+    );
+  }
+}
+*/
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Latihan Image"),),
+        body: Center(
+          child: Container(
+            width: 200,
+            height: 200,
+            padding: EdgeInsets.all(3),
+            child: Image(image: AssetImage("images/cotooo.jpg")),
+          )
         ),
       ),
     );
